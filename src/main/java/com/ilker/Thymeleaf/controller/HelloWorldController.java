145ca48faa2a710774b42f59d174fd9f5f6a7c1f
ilkerbyrktr <ilkerbayraktar1994@gmail.com>
2023-10-24 00:28:11 +0300

    Some Changes

--- src/main/java/com/ilker/Thymeleaf/controller/HelloWorldController.java
+++ src/main/java/com/ilker/Thymeleaf/controller/HelloWorldController.java
@@ -1,8 +1,12 @@
 package com.ilker.Thymeleaf.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Locale;
 
 @Controller
 public class HelloWorldController {
@@ -10,8 +14,13 @@ public class HelloWorldController {
     public String showForm(){
         return "helloworld-form";
     }
-    @GetMapping("/processForm")
-    public String processForm(){
+
+    @RequestMapping("/processFormVTwo")
+    public String letsShout(HttpServletRequest request, Model model){
+        String name = request.getParameter("studentName");
+        name=name.toUpperCase();
+        String result = "Selam! "+name;
+        model.addAttribute("message",result);
         return "helloworld";
     }
 }
