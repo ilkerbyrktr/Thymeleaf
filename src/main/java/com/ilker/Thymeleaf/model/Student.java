c9b75dab894f9447697d504aeb8e062cea7ad610
ilkerbyrktr <ilkerbayraktar1994@gmail.com>
2023-10-25 20:39:51 +0300

    Added @RequestParam("parameter name") support

--- src/main/java/com/ilker/Thymeleaf/model/Student.java
+++ src/main/java/com/ilker/Thymeleaf/model/Student.java
@@ -1,9 +1,39 @@
 package com.ilker.Thymeleaf.model;
 
+import java.util.List;
+
 public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
+    private String favouriteLanguage;
+
+    private List<String> favOS;
+
+    public List<String> getFavOS() {
+        return favOS;
+    }
+
+    public void setFavOS(List<String> favOS) {
+        this.favOS = favOS;
+    }
+
+    public String getFavouriteLanguage() {
+        return favouriteLanguage;
+    }
+
+    public void setFavouriteLanguage(String favouriteLanguage) {
+        this.favouriteLanguage = favouriteLanguage;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     public Student() {
     }
